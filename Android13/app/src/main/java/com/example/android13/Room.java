5951f618daf7169943d6bd409aa02b3c2c8f7b2d
Android-Trainer <service@web-academy.com.ua>
2019-01-23 20:46:27 +0200

    17
    17

---
+++ Android13/app/src/main/java/com/example/android13/Room.java
@@ -0,0 +1,21 @@
+package com.example.android13;
+
+public class Room {
+    private String description;
+
+    public Room() {
+    }
+
+    public Room(String description) {
+        this.description = description;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
